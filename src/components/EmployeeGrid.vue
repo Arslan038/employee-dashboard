330ba352fae1ad263eb93d2f223de142fa4ebec9
Arslan038 <arslanahmed983@gmail.com>
2025-03-08 13:46:34 +0500

    added sort function

--- src/components/EmployeeGrid.vue
+++ src/components/EmployeeGrid.vue
@@ -6,25 +6,31 @@
       <table class="min-w-full bg-white border rounded-lg overflow-hidden">
         <thead class="bg-gray-100">
           <tr>
-            <th class="text-left py-3 px-3">Full Name</th>
-            <th class="text-left py-3 px-3">Occupation</th>
-            <th class="text-left py-3 px-3">Department</th>
-            <th class="text-left py-3 px-3">Date of Employment</th>
-            <th class="text-left py-3 px-3">Termination Date</th>
-            <th class="text-left py-3 px-3">Actions</th>
+            <th v-for="column in columns" :key="column" class="py-3 px-3 hover:cursor-pointer"
+              @click="actions.sort(column)">
+              <div class="flex justify-start items-center">
+                <span class="text-sm">{{ column.text }}</span>
+                <img v-if="column.sortable" src="../assets/icons/sort.svg" class="ml-1 h-4 w-4" />
+              </div>
+            </th>
           </tr>
         </thead>
         <tbody>
           <tr v-for="employee in filteredEmployees" :key="employee.id" class="hover:bg-gray-50">
-            <td class="text-left py-3 px-3 border-t">{{ employee.fullName }}</td>
-            <td class="text-left py-3 px-3 border-t">{{ employee.occupation }}</td>
-            <td class="text-left py-3 px-3 border-t">{{ employee.department }}</td>
-            <td class="text-left py-3 px-3 border-t">{{ employee.dateOfEmployment }}</td>
-            <td class="text-left py-3 px-3 border-t">{{ employee.terminationDate || 'N/A' }}</td>
-            <td class="text-left py-3 px-3 border-t space-x-2">
-              <button class="text-blue-500 hover:text-blue-700">View</button>
-              <button class="text-green-500 hover:text-green-700">Edit</button>
-              <button class="text-red-500 hover:text-red-700">Delete</button>
+            <td class="text-left text-sm py-2 px-3 border-t">{{ employee.fullName }}</td>
+            <td class="text-left text-sm py-2 px-3 border-t">{{ employee.occupation }}</td>
+            <td class="text-left text-sm py-2 px-3 border-t">{{ employee.department }}</td>
+            <td class="text-left text-sm py-2 px-3 border-t">{{ actions.getEmploymentStatus(employee.dateOfEmployment)
+            }}</td>
+            <td class="text-left text-sm py-2 px-3 border-t">{{ actions.getTerminationStatus(employee.terminationDate)
+              || 'N/A' }}</td>
+            <td class="text-left text-sm py-2 px-3 border-t space-x-2">
+              <button class="text-blue-500 hover:text-blue-700 hover:cursor-pointer"
+                @click="actions.viewEmployee(employee.id)">View</button>
+              <button class="text-green-500 hover:text-green-700 hover:cursor-pointer"
+                @click="actions.editEmployee(employee.id)">Edit</button>
+              <button class="text-red-500 hover:text-red-700 hover:cursor-pointer"
+                @click="actions.deleteEmployee(employee.id)">Delete</button>
             </td>
           </tr>
         </tbody>
@@ -39,12 +45,16 @@
           <div><strong>Full Name:</strong> {{ employee.fullName }}</div>
           <div><strong>Occupation:</strong> {{ employee.occupation }}</div>
           <div><strong>Department:</strong> {{ employee.department }}</div>
-          <div><strong>Date of Employment:</strong> {{ employee.dateOfEmployment }}</div>
-          <div><strong>Termination Date:</strong> {{ employee.terminationDate || 'N/A' }}</div>
+          <div><strong>Date of Employment:</strong> {{ actions.getEmploymentStatus(employee.dateOfEmployment) }}</div>
+          <div><strong>Termination Date:</strong> {{ actions.getTerminationStatus(employee.terminationDate) || 'N/A' }}
+          </div>
           <div class="flex space-x-2">
-            <button class="text-blue-500 hover:text-blue-700">View</button>
-            <button class="text-green-500 hover:text-green-700">Edit</button>
-            <button class="text-red-500 hover:text-red-700">Delete</button>
+            <button class="text-blue-500 hover:text-blue-700 hover:cursor-pointer"
+              @click="actions.viewEmployee(employee.id)">View</button>
+            <button class="text-green-500 hover:text-green-700 hover:cursor-pointer"
+              @click="actions.editEmployee(employee.id)">Edit</button>
+            <button class="text-red-500 hover:text-red-700 hover:cursor-pointer"
+              @click="actions.deleteEmployee(employee.id)">Delete</button>
           </div>
         </div>
       </div>
@@ -57,25 +67,97 @@ import { employees } from '../utils/employees';
 import SearchInput from './SearchInput.vue';
 import { computed, ref, reactive } from 'vue';
 
+const columns = [
+  { key: 'fullName', text: 'Full Name', sortable: true },
+  { key: 'occupation', text: 'Occupation', sortable: true },
+  { key: 'department', text: 'Department', sortable: true },
+  { key: 'dateOfEmployment', text: 'Date of Employment', sortable: true },
+  { key: 'terminationDate', text: 'Termination Date', sortable: true },
+  { key: 'actions', text: 'Actions', sortable: false }
+];
+
 const employeeList = reactive({
   items: [...employees]
 });
 
 const searchQuery = ref('');
+const sortKey = ref('');
+const sortDirection = ref('asc');
 
 const filteredEmployees = computed(() => {
-  if (!searchQuery.value) return employeeList.items;
+  let filtered = [];
 
-  return employeeList.items.filter((employee) =>
+  // Filter Employees
+  filtered = employeeList.items.filter((employee) =>
     Object.values(employee).some((value) =>
       String(value).toLowerCase().includes(searchQuery.value.toLowerCase())
     )
   );
+
+  // Sort Employees
+  if (sortKey.value) {
+    filtered.sort((a, b) => {
+      const aValue = a[sortKey.value];
+      const bValue = b[sortKey.value];
+
+      if (aValue < bValue) return sortDirection.value === 'asc' ? -1 : 1;
+      if (aValue > bValue) return sortDirection.value === 'asc' ? 1 : -1;
+      return 0;
+    });
+  }
+
+  // If Search is empty and no column sorted return default list
+  if (!searchQuery.value && !sortKey.value) return employeeList.items;
+
+  return filtered;
 });
 
 const actions = {
   onEmployeeSearch(query) {
     searchQuery.value = query;
+  },
+
+  sort(column) {
+    if (column && column.sortable) {
+      const key = column.key;
+
+      if (sortKey.value === key) {
+        sortDirection.value = sortDirection.value === 'asc' ? 'desc' : 'asc';
+      } else {
+        sortKey.value = key;
+        sortDirection.value = 'asc';
+      }
+    }
+
+  },
+
+  // Get employment status
+  getEmploymentStatus(date) {
+    const today = new Date();
+    const employmentDate = new Date(date);
+    if (employmentDate > today) return 'Employed soon';
+    return 'Currently employed';
+  },
+
+  // Get termination status
+  getTerminationStatus(date) {
+    if (!date) return 'N/A';
+    const today = new Date();
+    const terminationDate = new Date(date);
+    if (terminationDate > today) return 'To be terminated';
+    return 'Terminated';
+  },
+
+  viewEmployee(id) {
+    console.log('View : ', id);
+  },
+
+  editEmployee(id) {
+    console.log('Edit : ', id);
+  },
+
+  deleteEmployee(id) {
+    console.log('Delete : ', id)
   }
 }
 </script>
