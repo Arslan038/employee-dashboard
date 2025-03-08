0701bd76d2589df01ce550e606b0e598d06703cc
Arslan038 <arslanahmed983@gmail.com>
2025-03-08 23:55:57 +0500

    alert updated

--- src/components/ui/Alert.vue
+++ src/components/ui/Alert.vue
@@ -1,5 +1,5 @@
 <template>
-    <div v-if="appStore.notification.show" id="alert" class="fixed top-10 right-10 mx-auto w-auto text-center text-white">
+    <div v-if="appStore.notification.show" id="alert" class="fixed z-100 top-10 right-10 mx-auto w-auto text-center text-white">
         <!-- Alert Content -->
         <div class="inline-block px-6 py-2 rounded-lg shadow-lg" :class="appStore.notification.type === 'success' ? 'bg-green-800' : 'bg-red-800'">
             <span id="alert-message">{{ appStore.notification.message }}</span>
