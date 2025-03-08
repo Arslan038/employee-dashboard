69dc9abd82e8c7dd644d450c2dca19250d5e649b
Arslan038 <arslanahmed983@gmail.com>
2025-03-08 12:48:33 +0500

    added search component

---
+++ src/components/SearchInput.vue
@@ -0,0 +1,16 @@
+<template>
+    <input v-model="query" type="text" placeholder="Filter employees..."
+        class="p-2 border rounded w-full focus:outline-none focus:ring-2 focus:ring-blue-500" />
+</template>
+
+<script setup>
+import { ref, watch } from 'vue';
+
+const emit = defineEmits(['onSearch']);
+
+const query = ref('');
+
+watch(query, (val) => {
+    emit('onSearch', val);
+})
+</script>
